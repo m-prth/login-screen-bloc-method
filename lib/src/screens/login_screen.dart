5d6c088385101d6a719c28b9359aee401ddbe846
Parth Mistry <imparthmistry@gmail.com>
2019-11-09 20:04:38 +0530

    Add files via upload

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Container(margin: EdgeInsets.only(top: 50.0)),
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ));
+  }
+}
+
+Widget emailField() {
+  return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      });
+}
+
+Widget passwordField() {
+  return StreamBuilder(
+    stream: bloc.password,
+    builder: (context, snapshot) {
+      return TextField(
+        onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          )
+      );
+    },
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('login'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
+}
